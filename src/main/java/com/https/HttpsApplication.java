d9e3af4b295ee538faee79f331dacc29733d0ed8
xiebanglin <2419507265@qq.com>
2020-12-25 21:43:03 +0800

    https请求

---
+++ src/main/java/com/https/HttpsApplication.java
@@ -0,0 +1,35 @@
+package com.https;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HttpsApplication {
+
+	@Value("${server.http-port}")
+	private int httpPort;
+	
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(httpPort);
+        connector.setScheme("http");
+        connector.setSecure(false);
+		return connector;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpsApplication.class, args);
+	}
+}
